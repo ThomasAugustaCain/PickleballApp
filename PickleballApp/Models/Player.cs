0ef84fda6f8fbce74e7fccdc27873d21f3ec4735
Augusta Cain <tacain@senators.ws.edu>
2025-11-23 11:27:59 -0500

    I completed the bare minimum for the player class

--- PickleballApp/Models/Player.cs
+++ PickleballApp/Models/Player.cs
@@ -10,10 +10,11 @@ namespace PickleballApp.Models
         [Required(ErrorMessage = "Please enter a name.")]
         public string Name { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Please enter a birth date.")]
-        //[Range(DateTime.Today.AddYears(-100), DateTime.Today.AddYears(-5), ErrorMessage = "A Player must be a least 5 years old.")]
-        // I need to create a custom validation type to perform the above code
-        public DateTime BirthDate { get; set; }
+        // I will add this at a later date
+        //[Required(ErrorMessage = "Please enter a birth date.")]
+        ////[Range(DateTime.Today.AddYears(-100), DateTime.Today.AddYears(-5), ErrorMessage = "A Player must be a least 5 years old.")]
+        //// I need to create a custom validation type to perform the above code
+        //public DateTime BirthDate { get; set; }
 
         public string? Gender { get; set; }
 
